03807160c22c41ea25d299f5ce313ee556a1a403
Piotr Majcher <piotrekxyz@tlen.pl>
2018-08-22 20:34:11 +0200

    start

---
+++ SieveOfEratosthenes/Classes/Calculations.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SieveOfEratosthenes.Classes
+{
+	static class Calculations
+	{
+		public static void SzukajRownolegleLP(long poczatekPrzedzialu, long koniecPrzedzialu)
+		{
+			long licznik = 0;
+			Stopwatch timer = new Stopwatch();
+			timer.Start();
+			Parallel.For(poczatekPrzedzialu, koniecPrzedzialu + 1, i =>
+			{
+				if (CzyLiczbaJestPierwsza(i))
+					Interlocked.Increment(ref licznik);
+			});
+			Console.WriteLine(string.Format(CultureInfo.InvariantCulture,
+				"W przedziale {0:0,0} - {1:0,0} jest\n{2:0,0} liczb pierwszych, policzono Równolegle w {3} s\n", poczatekPrzedzialu, koniecPrzedzialu, licznik, (double)timer.ElapsedMilliseconds / 1000));
+		}
+
+		public static void SzukajSekwencyjnieLP(long poczatekPrzedzialu, long koniecPrzedzialu)
+		{
+			long licznik = 0;
+			Stopwatch timer = new Stopwatch();
+			timer.Start();
+			for (long i = poczatekPrzedzialu; i <= koniecPrzedzialu; i++)
+				if (CzyLiczbaJestPierwsza(i))
+					licznik++;
+			Console.WriteLine(string.Format(CultureInfo.InvariantCulture,
+				"W przedziale {0:0,0} - {1:0,0} jest\n{2:0,0} liczb pierwszych, policzono Sekwencyjnie w {3} s\n", poczatekPrzedzialu, koniecPrzedzialu, licznik, (double)timer.ElapsedMilliseconds / 1000));
+		}
+
+		public static void SzukajSitemEratostenesaLP(long poczatek, long koniec)
+		{
+			Stopwatch timer = new Stopwatch();
+			timer.Start();
+			bool[] tablica = new bool[koniec + 1];  // żeby się indeksy zgadzały z prawdziwymi liczbami
+			WykreslajSitem(ref tablica, koniec);
+			long iloscLP = poczatek == 2 ? tablica.Count(n => n == false) : PoliczLiczbyPierwszeWTablicy(poczatek, ref tablica);
+			Console.WriteLine(string.Format(CultureInfo.InvariantCulture,
+				"W przedziale {0:0,0} - {1:0,0} jest\n{2:0,0} liczb pierwszych, policzono Sitem Eratostenesa w {3} s\n", poczatek, koniec, iloscLP, (double)timer.ElapsedMilliseconds / 1000));
+		}
+
+		static void WykreslajSitem(ref bool[] tablica, long koniecPrzedzialu)
+		{
+			tablica[0] = tablica[1] = true; // pierwsze dwa elementy olewamy
+			for (long i = 2; i <= Math.Sqrt(tablica.Length - 1); i++)
+			{
+				if (tablica[i] == false)
+				{
+					long j = i + i;
+					while (j <= koniecPrzedzialu)
+					{
+						tablica[j] = true;
+						j += i;
+					}
+				}
+			}
+		}
+
+		static long PoliczLiczbyPierwszeWTablicy(long poczatekPrzedzialu, ref bool[] tablica)
+		{
+			long licznik = 0;
+			for (long i = poczatekPrzedzialu; i < tablica.Length; i++)
+				if (tablica[i] == false)
+					licznik++;
+			return licznik;
+		}
+
+		static bool CzyLiczbaJestPierwsza(long liczba)
+		{
+			if (liczba < 2)
+				return false;
+			if (liczba == 2 || liczba == 3)
+				return true;
+			for (long i = 2; i <= Math.Sqrt(liczba); i++)
+				if (liczba % i == 0)
+					return false;
+			return true;
+		}
+	}
+}
--- SieveOfEratosthenes/Program.cs
+++ SieveOfEratosthenes/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using SieveOfEratosthenes.Classes;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SieveOfEratosthenes
 {
 	class Program
 	{
+		static long poczatekPrzedzialu = 33;
+		static long koniecPrzedzialu = 17389;
+
 		static void Main(string[] args)
 		{
+			while (true)
+			{
+				PobierzDane(ref poczatekPrzedzialu, ref koniecPrzedzialu);
+				Calculations.SzukajSitemEratostenesaLP(poczatekPrzedzialu, koniecPrzedzialu);
+				Calculations.SzukajRownolegleLP(poczatekPrzedzialu, koniecPrzedzialu);
+				Calculations.SzukajSekwencyjnieLP(poczatekPrzedzialu, koniecPrzedzialu);
+			}
+		}
+
+		static void PobierzDane(ref long poczatekPrzedzialu, ref long koniecPrzedzialu)
+		{
+			Console.WriteLine("\nWpisz początek i koniec przedziału, oddzielone spacją i wciśnij ENTER");
+			long[] tab = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+			poczatekPrzedzialu = tab[0];
+			koniecPrzedzialu = tab[1];
+			Console.WriteLine();
 		}
 	}
 }
